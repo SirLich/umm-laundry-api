8efb68ac7b9f5f51e72cdfab6dcb0886ea79d842
SirLich <lrkoehler77@gmail.com>
2019-12-11 22:38:40 -0600

    Update views.py to serve machines via webhook changes, rather than static
    This change represents moving the API from a simple mockup into an actual functional backend.

--- app/views.py
+++ app/views.py
@@ -5,6 +5,22 @@ from flask import make_response
 from app import app
 import json
 
+# EXAMPLE MACHINE JSON
+# {
+# 	"id": "c4499d38-f937-11e9-8874-902b34113095",
+# 	"name": "chummy-beige-dollar",
+# 	"type": "washer",
+# 	"running": false,
+# 	"status": "normal"
+# 	"room_id": "gay"
+# }
+
+# Local list of machines. This list is generated based on the updates to /update_machine
+# A fresh instantiation of the app will have no machines. Machines can be added manually by pushing an "I'm awake" request.
+# Otherwise, machines will be added as they change state.
+machines = {}
+
+# Home route, used to provide information about the API
 @app.route('/')
 def home():
     m = """
@@ -14,33 +30,46 @@ def home():
     /machines \n
     /rooms/{room}/machines \n
     /machines/{machine} \n
+    /update_machine
     """
 
     response = make_response(m)
     response.headers["content-type"] = "text/plain"
     return response
 
+# Returns static list of rooms. This list can be edited as needed in the config
 @app.route('/rooms')
 def get_rooms():
-    return send_from_directory(app.config['STATIC_FOLDER'], "rooms.json")
+    return jsonify(app.config['ROOMS'])
 
+# Returns all machines that the system knows about.
 @app.route('/machines')
 def get_machines():
-    return send_from_directory(app.config['STATIC_FOLDER'], "machines.json")
+    return jsonify(machines)
 
+# Returns machines, sorted by room.
 @app.route("/rooms/<room>/machines")
 def get_room(room):
-    with open(app.config['STATIC_FOLDER'] + "/machines.json", 'r') as f:
-        input_dict = json.load(f)
-
-    output_dict = [x for x in input_dict if x['room_id'] == room]
-
+    output_dict = [x for x in machines if x['room_id'] == room]
     return jsonify(output_dict)
 
+# Returns a specific machine.
 @app.route('/machines/<machine>')
 def get_machine(machine):
-    with open(app.config['STATIC_FOLDER'] + "/machines.json", 'r') as f:
-        input_dict = json.load(f)
-
-    output_dict = [x for x in input_dict if x['id'] == machine]
+    output_dict = [x for x in machines if x['id'] == machine]
     return jsonify(output_dict)
+
+# Update the status of new machines. 
+# New machines can be "added" by simply bringing them online, and setting up IFTTT rules for it.
+@app.route('/update_machine', methods=['POST'])
+def update_machine():
+    if request.method == 'POST':
+        try:
+            raw = request.json
+            id = raw["id"]
+            machines[id] = raw
+            return '', 200
+        except:
+            "Something went wrong"
+    else:
+        abort(400)
--- config.py
+++ config.py
@@ -1,3 +1,33 @@
 # Enable Flask's debugging features. Should be False in production
 DEBUG = True
 STATIC_FOLDER="/root/umm-laundry-api/app/static"
+ROOMS = [
+  {
+  	"id": "gay",
+  	"name": "Gay Hall"
+  },
+  {
+  	"id": "independence",
+  	"name": "Independence Hall"
+  },
+  {
+	"id": "green_prairie",
+	"name": "Green Prairie Hall"
+  },
+  {
+	"id": "the_apartments",
+	"name": "The Apartments"
+  },
+  {
+	"id": "blakely",
+	"name": "Blakely Hall"
+  },
+  {
+	"id": "pine",
+	"name": "Pine Hall"
+  },
+  {
+	"id": "spooner",
+	"name": "Spooner Hall"
+  }
+]
